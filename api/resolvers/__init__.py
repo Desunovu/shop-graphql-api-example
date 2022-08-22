947107e3f194e2cd6fbfb64cbf1dc0c1949e99f9
desunovu <desunovu@gmail.com>
2022-08-23 00:30:31 +0300

    Рефракторинг структуры graphql схемы

--- api/resolvers/__init__.py
+++ api/resolvers/__init__.py
@@ -1,5 +1,7 @@
+# 4 query
 from .login_user import resolve_login_user
 from .get_user import resolve_get_user
 from .get_users import resolve_get_users
 
+# mutation
 from .create_user import resolve_create_user
