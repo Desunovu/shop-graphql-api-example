e13d7f7894f8018b5014c7e3a37cc669f4e46f52
desunovu <desunovu@gmail.com>
2022-08-22 18:42:49 +0300

    Добавление UnauthorizedError для token_required

--- api/common/auth.py
+++ api/common/auth.py
@@ -4,33 +4,34 @@ import jwt
 from flask import request
 
 from api import app, db
-from api.models import User, Role
-from api.common.common import CUSTOMER_ACCESS_LEVEL
+from api.models import User
+from api.common.enum_types import RoleEnum
+
+
+class UnauthorizedError(Exception):
+    extension = {"code": 401}
 
 
 # TODO переписать для резолверов
-def token_required(access_lvl=CUSTOMER_ACCESS_LEVEL):
+def token_required(role=RoleEnum.CUSTOMER.value):
     def token_required_wrapper(func):
         @wraps(func)
         def decorated_view(*args, **kwargs):
             token = None
             current_user = None
-
             if 'x-access-token' in request.headers:
                 token = request.headers['x-access-token']
             else:
-                raise Exception
+                raise UnauthorizedError("Unauthorized")
 
             try:
-                user_data = jwt.decode(jwt=token, key=app.secret_key, algorithms='HS256')
-                user = db.session.query(User).get(user_data['id'])
-                user_access_level = db.session.query(Role).get(user.role).access_level
-                if user_access_level >= access_lvl:
-                    return func(current_user, *args, **kwargs)
-                else:
-                    raise Exception
-            except Exception:
-                raise Exception
+                data = jwt.decode(jwt=token, key=app.secret_key, algorithms='HS256')
+            except jwt.exceptions.InvalidTokenError:
+                raise UnauthorizedError("Unauthorized")
+
+            current_user = db.session.query(User).get(data['id'])
+            if not current_user:
+                raise UnauthorizedError("Unauthorized")
 
         return decorated_view
     return token_required_wrapper
