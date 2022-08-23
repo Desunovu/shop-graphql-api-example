91c2e9c6a2d93a5a5acd320974c28b6989d0ccc4
desunovu <desunovu@gmail.com>
2022-08-24 01:10:59 +0300

    Рефракторинг запроса loginUser

--- api/resolvers/login_user.py
+++ api/resolvers/login_user.py
@@ -10,9 +10,10 @@ from api import app, db
 
 def resolve_login_user(_obj, _info, email, password):
     user = db.session.query(User).filter(User.email == email).first()
-    if user and check_password_hash(user.password, password):
-        token = jwt.encode(payload={"id": user.id, "exp": datetime.datetime.now() + datetime.timedelta(days=7)},
-                           key=app.secret_key, algorithm="HS256")
-        return create_result(token=token)
-    else:
+
+    if not (user and check_password_hash(user.password, password)):
         return create_result(status=False, errors=[Errors.WRONG_EMAIL_OR_PASSWORD])
+
+    token = jwt.encode(payload={"id": user.id, "exp": datetime.datetime.now() + datetime.timedelta(days=7)},
+                       key=app.secret_key, algorithm="HS256")
+    return create_result(token=token)
