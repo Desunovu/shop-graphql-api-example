b48fbbf959190f85c5eb407057e2da45751a941d
desunovu <desunovu@gmail.com>
2022-09-20 10:35:04 +0300

    Добавление запроса getCategories

--- api/queries/__init__.py
+++ api/queries/__init__.py
@@ -1,6 +1,6 @@
 from ariadne import ObjectType
 
-from . import cart, product, signin, user
+from . import cart, product, signin, user, category
 
 query = ObjectType("Query")
 
@@ -17,3 +17,6 @@ query.set_field("getUsers", user.resolve_get_users)
 # Product/Products
 query.set_field("getProduct", product.resolve_get_product)
 query.set_field("getProducts", product.resolve_get_products)
+
+# Category/Categories
+query.set_field("getCategories", category.resolve_get_categories)
---
+++ api/queries/category.py
@@ -0,0 +1,9 @@
+from api import db
+from api.common import create_result, token_required, Errors, Roles
+from api.models import Category
+
+
+@token_required()
+def resolve_get_categories(_obj, _info, **kwargs):
+    categories = db.session.query(Category).all()
+    return create_result(categories=categories)
