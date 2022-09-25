ff362b90f9b3bbd6a22ac711211245d26f628db1
desunovu <desunovu@gmail.com>
2022-09-25 04:58:06 +0300

    Добавление фильтра к запросу getProducts
    - добавлен фильтр по categoryId

--- api/queries/product.py
+++ api/queries/product.py
@@ -1,6 +1,6 @@
 from api import db
 from api.common import token_required, create_result, Errors
-from api.models import Product
+from api.models import Product, ProductCategory
 
 
 @token_required()
@@ -13,7 +13,21 @@ def resolve_get_product(_obj, _info, **kwargs):
 
 
 @token_required()
-def resolve_get_products(_obj, _info):
-    products = db.session.query(Product).all()
+def resolve_get_products(_obj, _info, **kwargs):
+    # Выражение
+    stmt = db.session.query(Product)
+
+    # Присутствует фильтр по категории
+    try:
+        stmt = stmt.join(ProductCategory, Product.id == ProductCategory.product_id).filter(
+            ProductCategory.category_id == kwargs["input"]["filter"]["categoryId"]
+        )
+    except KeyError:
+        pass
+
+    # Поиск в БД
+    products = stmt.all()
+    if not products:
+        return create_result(status=False, errors=[Errors.OBJECT_NOT_FOUND])
 
     return create_result(products=[product for product in products])
