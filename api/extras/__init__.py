0e9eb9bf30191e3a1cea9e8e9cded4472dea16c1
desunovu <desunovu@gmail.com>
2022-10-06 20:10:36 +0300

    Добавление функционала изменения статуса заказа
    + запрос updateOrderStatus
    + изменение даты заказа: creation/completion-date
    + фикс создания заказа (без коммита не обновлялись товары)
    + ренейминг graphql файлов

--- api/extras/__init__.py
+++ api/extras/__init__.py
@@ -1,3 +1,3 @@
 from .auth_wrapper import token_required
-from .common_constants import Roles, Errors, UnauthorizedError, ForbiddenError, NotEnoughProduct
+from .common_constants import Roles, Errors, UnauthorizedError, ForbiddenError, NotEnoughProduct, OrderStatus
 from .creation_utils import create_result, create_simple_result, create_error
--- api/mutations/order.py
+++ api/mutations/order.py
@@ -3,7 +3,7 @@ from sqlalchemy import update
 
 from api import db
 from api.models import CartLine, OrderLine, Order, User, Product
-from api.extras import token_required, create_result, create_error, Errors, Roles, NotEnoughProduct
+from api.extras import token_required, create_result, create_error, OrderStatus, Errors, Roles, NotEnoughProduct
 
 
 @token_required()
@@ -40,8 +40,8 @@ def resolve_create_order(_obj, info, **kwargs):
     # запись в таблице orders
     new_order = Order(
         user_id=info.context.current_user.id,
-        date=datetime.date.today(),
-        completed=False
+        creation_date=datetime.date.today(),
+        status=OrderStatus.PROCESSING
     )
     new_order.delivery_address = info.context.current_user.address
     if "deliveryAddress" in kwargs:
@@ -50,24 +50,71 @@ def resolve_create_order(_obj, info, **kwargs):
     db.session.commit()
 
     # Перенос в таблицу orderlines, очистка корзины, резервация, уменьшение доступного количества товара
-    # Откат записи в orders и orderlines если произошла ошибка переноса
-    try:
-        db.session.add_all(
-            [OrderLine(**cartline.to_dict(), order_id=new_order.id) for cartline in cartlines]
-        )
-        db.session.execute(update_products_stmt)
-        cartlines.delete()
-    except Exception as ex:
-        print(ex)
-        db.session.rollback()
-        db.session.delete(new_order)
-        db.session.commit()
-        return create_result(status=False, errors=[Errors.ORDER_CREATION_EXCEPTION])
+    # TODO TRY-EXCEPT
+    db.session.add_all(
+        [OrderLine(**cartline.to_dict(), order_id=new_order.id) for cartline in cartlines]
+    )
+    db.session.commit()
+    print(db.session.execute(update_products_stmt).rowcount)
+    cartlines.delete()
 
     db.session.commit()
     return create_result(order=new_order)
 
 
 @token_required(allowed_roles=[Roles.ADMIN])
-def resolve_close_order(_obj, _info, **kwargs):
-    pass
+def resolve_update_order_status(_obj, info, **kwargs):
+    new_status = kwargs["orderStatus"]
+
+    # Выражение для уменьшения поля products.reserved при отправке
+    update_shipped_products_stmt = update(Product.__table__)\
+        .values(
+            reserved=Product.__table__.c.reserved - OrderLine.__table__.c.amount
+        )\
+        .where(OrderLine.__table__.c.user_id == info.context.current_user.id)\
+        .where(OrderLine.__table__.c.product_id == Product.__table__.c.id) \
+        .execution_options(synchronize_session="fetch")
+
+    # Выражение для изменения products при отмене заказа
+    update_cancelled_products_stmt = update(Product.__table__) \
+        .values(
+            amount=Product.__table__.c.amount + OrderLine.__table__.c.amount,
+            reserved=Product.__table__.c.reserved - OrderLine.__table__.c.amount
+        ) \
+        .where(OrderLine.__table__.c.user_id == info.context.current_user.id) \
+        .where(OrderLine.__table__.c.product_id == Product.__table__.c.id) \
+        .execution_options(synchronize_session="fetch")
+
+    # Поиск заказа, вернуть ошибку если заказа не существует
+    order = db.session.query(Order).get(kwargs["id"])
+    if not order:
+        return create_result(status=False, errors=[Errors.OBJECT_NOT_FOUND])
+
+    # [PROCESSING] (заглушка)
+    if new_status == OrderStatus.PROCESSING:
+        return create_result(status=False, errors=[Errors.WRONG_ORDER_STATUS])
+
+    # [SHIPPING] возможен после processing, очищаются строки резерва
+    if new_status == OrderStatus.SHIPPING:
+        if order.status != OrderStatus.PROCESSING:
+            return create_result(status=False, errors=[Errors.WRONG_ORDER_STATUS])
+        db.session.execute(update_shipped_products_stmt)
+        order.status = new_status
+
+    # [DONE] возможен после shipping
+    if new_status == OrderStatus.DONE:
+        if order.status != OrderStatus.SHIPPING:
+            return create_result(status=False, errors=[Errors.WRONG_ORDER_STATUS])
+        order.status = new_status
+        order.completion_date = datetime.date.today()
+
+    # [CANCELLED] возможен после processing, вернуть товары из резерва в доступные
+    if new_status == "CANCELLED":
+        if order.status != OrderStatus.PROCESSING:
+            return create_result(status=False, errors=[Errors.WRONG_ORDER_STATUS])
+        db.session.execute(update_cancelled_products_stmt)
+        order.status = new_status
+        order.completion_date = datetime.date.today()
+
+    db.session.commit()
+    return create_result(order=order)
