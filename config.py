7890627c8c9fbf8ab85567a586e5d7fa66d3ce54
desunovu <desunovu@gmail.com>
2023-01-08 01:25:25 +0300

    Добавление проверки бакита при деплое

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class BaseConfig:
 
     # Minio
     MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT")
-    MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
-    MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
+    MINIO_ACCESS_KEY = os.environ.get("MINIO_ROOT_USER")
+    MINIO_SECRET_KEY = os.environ.get("MINIO_ROOT_PASSWORD")
     PRODUCTS_BUCKET = os.environ.get("PRODUCTS_BUCKET")
 
 
